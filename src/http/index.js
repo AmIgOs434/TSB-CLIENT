abf780a48ca46b2c1da4c28e52d3cc16236fe821
AmIgOs434 <104652966+AmIgOs434@users.noreply.github.com>
2023-07-15 11:38:10 +0300

    Update index.js

--- src/http/index.js
+++ src/http/index.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 const $host = axios.create({
-    baseURL: process.env.REACT_APP_API_URL
+    baseURL: 'https://the-same-baze.onrender.com/'
 })
 
 const $authHost = axios.create({
-    baseURL: process.env.REACT_APP_API_URL
+    baseURL: 'https://the-same-baze.onrender.com/'
 })
 
 
